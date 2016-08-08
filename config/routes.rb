11f864b1091e15982d174bee4e3b55702f1e0967
Lilly Piri <lilly@lillypiri.com>
2016-08-08 18:09:49 +1000

    About added as static page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
 
   resources :articles, only: [:show, :index]
+  get "about", to: "pages#about"
   root 'articles#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
