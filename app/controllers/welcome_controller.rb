76ce82a8349854da534dfa2ab79a0c8609afc452
Lilly Piri <lilly@lillypiri.com>
2017-04-22 17:52:11 +1000

    Sitemap added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
+  layout false, only: [:sitemap]
+
+
   def index
+  end
+
+
+  def sitemap
+    @articles = Article.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :articles, only: [:show, :index]
   get "about", to: "pages#about"
+  get "sitemap.xml", to: "welcome#sitemap"
   root 'articles#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
